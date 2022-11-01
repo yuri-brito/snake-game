c58e448b23e51a7fe3c3929edd55dacca7167ad0
Yuri Brito <yurisb85@gmail.com>
2022-11-01 09:55:30 -0300

    Correção touch botao start

--- touch.js
+++ touch.js
@@ -2,7 +2,7 @@
 let x1;
 let y1;
 function process_touchstart(ev){
-
+    
     x1=ev.changedTouches[0].clientX
     y1=ev.changedTouches[0].clientY
     
@@ -13,26 +13,31 @@ function process_touchend(ev){
     if (Math.abs(y2-y1)>Math.abs(x2-x1)){
         if (y2>y1){
             let e = new KeyboardEvent('keydown',{'keyCode':40,'which':40});
-            //console.log(e);
             window.dispatchEvent(e);
         } else{
             let e = new KeyboardEvent('keydown',{'keyCode':38,'which':38});
-            //console.log(e);
             window.dispatchEvent(e);
         }
     }else{
         if (x2>x1){
             let e = new KeyboardEvent('keydown',{'keyCode':39,'which':39});
-            //console.log(e);
             window.dispatchEvent(e);
 
         } else{
             let e = new KeyboardEvent('keydown',{'keyCode':37,'which':37});
-            //console.log(e);
             window.dispatchEvent(e);
         }
     }
 
 }
+function botaoStartTouch(ev){
+    let e = new MouseEvent('click');
+    //console.log(e);
+    ev.target.dispatchEvent(e);
+}
+
 document.getElementsByTagName('body')[0].addEventListener('touchstart',process_touchstart)
 document.getElementsByTagName('body')[0].addEventListener('touchend',process_touchend)
+
+botaoStart=document.getElementById('start')
+botaoStart.addEventListener('touchstart',botaoStartTouch)
